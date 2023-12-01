c9ebd7776c8b84d3a886e4f8bdf4a9e013e4d7f2
Leevi10 <Leevi.ilander@seamk.fi>
2023-12-01 08:48:18 +0200

    Add project files.

---
+++ Sovelluskehitys_harkka/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Data.SqlClient;
+
+namespace Sovelluskehitys_harkka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string solun_arvo;
+        string polku = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\k2101792\\Documents\\Tietokanta.mdf;Integrated Security=True;Connect Timeout=30";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
